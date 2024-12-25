33d9a0a27647568437da218d76d9e2df51e577b9
Shahriar Parvez <xtniloy@gmail.com>
2024-12-26 02:19:04 +0600

    blank template

---
+++ resources/views/admin/sections/blank.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layout.main')
+@section('page-title')
+    Dashboard
+@endsection
+@section('content')
+    <div class="container-lg px-4">
+        <div class="fs-2 fw-semibold" data-coreui-i18n="dashboard">Dashboard</div>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="#" data-coreui-i18n="home">Home</a>
+                </li>
+                <li class="breadcrumb-item active"><span data-coreui-i18n="dashboard">Dashboard</span>
+                </li>
+            </ol>
+        </nav>
+
+        <!-- /.row-->
+    </div>
+@endsection
+
+
+
