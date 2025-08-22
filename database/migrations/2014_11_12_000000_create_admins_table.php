0217693669a33245f36527957c227b14ae6913e2
Shahriar Parvez <xtniloy@gmail.com>
2025-08-23 02:43:50 +0600

    admin panel migration update

--- database/migrations/2014_11_12_000000_create_admins_table.php
+++ database/migrations/2014_11_12_000000_create_admins_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->foreignId('user_id')->nullable()->constrained('users')->restrictOnDelete();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
