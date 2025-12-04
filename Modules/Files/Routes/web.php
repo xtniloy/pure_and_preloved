d87f59c92206d410e228e71d1cae0088e98256f0
Shahriar Parvez <xtniloy@gmail.com>
2025-12-04 16:25:07 +0600

    File module work in progress 2

--- Modules/Files/Routes/web.php
+++ Modules/Files/Routes/web.php
@@ -7,4 +7,13 @@ Route::middleware('web')
     ->prefix('files')
     ->group(function () {
         Route::get('/', [FilesController::class, 'index']);
+        Route::post('/upload-chunk', [FilesController::class, 'uploadChunk'])
+            ->name('admin.file.upload.chunk');
+        Route::get('/download/{fileId?}', [FilesController::class, 'download'])
+            ->name('admin.file.download');
+        Route::delete('/delete/{fileId?}', [FilesController::class, 'delete'])
+            ->name('admin.file.delete');
+
+        Route::get('/view/{fileId?}', [FilesController::class, 'view'])
+            ->name('admin.file.view');
     });
