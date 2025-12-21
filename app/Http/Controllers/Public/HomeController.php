e7376ba1551312833f15ff638b3342bef203d82d
Shahriar Parvez <xtniloy@gmail.com>
2025-12-22 02:25:24 +0600

    Import public assets

---
+++ app/Http/Controllers/Public/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('public.home');
+    }
+}
