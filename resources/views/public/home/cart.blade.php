196fef4ba102e22078762f8487d55d4afdffab4b
Shahriar Parvez <xtniloy@gmail.com>
2026-03-01 03:18:54 +0600

    Update cart page layout

--- resources/views/public/home/cart.blade.php
+++ resources/views/public/home/cart.blade.php
@@ -83,6 +83,11 @@
                                             </td>
                                         </tr>
                                     @endforeach
+                                    <tr>
+                                        <td colspan="4" class="text-end border-0"></td>
+                                        <td class="product-subtotal text-center"><strong>Total:</strong></td>
+                                        <td class="product-subtotal"><strong>${{ number_format($cartSubtotal, 2) }}</strong></td>
+                                    </tr>
                                 @endif
                                 </tbody>
                             </table>
@@ -95,7 +100,7 @@
                                     </div>
                                     <div class="cart-clear">
                                         <button type="submit">Update Shopping Cart</button>
-                                        <a href="#" onclick="event.preventDefault(); document.getElementById('clear-cart-form').submit();">Clear Shopping Cart</a>
+                                        <a href="{{ route('checkout.index') }}">Checkout</a>
                                     </div>
                                 </div>
                             </div>
@@ -110,87 +115,6 @@
                             </form>
                         @endforeach
                     @endif
-                    <form id="clear-cart-form" action="{{ route('cart.clear') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6 mb-lm-30px">
-                            <div class="cart-tax">
-                                <div class="title-wrap">
-                                    <h4 class="cart-bottom-title section-bg-gray">Estimate Shipping And Tax</h4>
-                                </div>
-                                <div class="tax-wrapper">
-                                    <p>Enter your destination to get a shipping estimate.</p>
-                                    <div class="tax-select-wrapper">
-                                        <div class="tax-select">
-                                            <label>
-                                                * Country
-                                            </label>
-                                            <select class="email s-email s-wid">
-                                                <option>Bangladesh</option>
-                                                <option>Albania</option>
-                                                <option>Åland Islands</option>
-                                                <option>Afghanistan</option>
-                                                <option>Belgium</option>
-                                            </select>
-                                        </div>
-                                        <div class="tax-select">
-                                            <label>
-                                                * Region / State
-                                            </label>
-                                            <select class="email s-email s-wid">
-                                                <option>Bangladesh</option>
-                                                <option>Albania</option>
-                                                <option>Åland Islands</option>
-                                                <option>Afghanistan</option>
-                                                <option>Belgium</option>
-                                            </select>
-                                        </div>
-                                        <div class="tax-select mb-25px">
-                                            <label>
-                                                * Zip/Postal Code
-                                            </label>
-                                            <input type="text" />
-                                        </div>
-                                        <button class="cart-btn-2" type="submit">Get A Quote</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 mb-lm-30px">
-                            <div class="discount-code-wrapper">
-                                <div class="title-wrap">
-                                    <h4 class="cart-bottom-title section-bg-gray">Use Coupon Code</h4>
-                                </div>
-                                <div class="discount-code">
-                                    <p>Enter your coupon code if you have one.</p>
-                                    <form>
-                                        <input type="text" required="" name="name" />
-                                        <button class="cart-btn-2" type="submit">Apply Coupon</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-12 mt-md-30px">
-                            <div class="grand-totall">
-                                <div class="title-wrap">
-                                    <h4 class="cart-bottom-title section-bg-gary-cart">Cart Total</h4>
-                                </div>
-                                <h5>Total products <span>${{ number_format($cartSubtotal, 2) }}</span></h5>
-                                <div class="total-shipping">
-                                    <h5>Total shipping</h5>
-                                    <ul>
-                                        <li><input type="checkbox" /> Standard <span>$20.00</span></li>
-                                        <li><input type="checkbox" /> Express <span>$30.00</span></li>
-                                    </ul>
-                                </div>
-                                <h4 class="grand-totall-title">Grand Total <span>${{ number_format($cartSubtotal, 2) }}</span></h4>
-                                @if(!empty($cartItems))
-                                    <a href="{{ route('checkout.index') }}">Proceed to Checkout</a>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
